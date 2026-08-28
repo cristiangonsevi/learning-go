51d4cce56548d212f3a7f903f0a2aa60093fc4fb
Cristian Gonzalez <albertsevilla1996@gmail.com>
2026-08-27 23:13:41 -0600

    feat: add shortcode genreation function

--- url-shortener/internal/services/url_service.go
+++ url-shortener/internal/services/url_service.go
@@ -2,6 +2,7 @@ package services
 
 import (
 	"context"
+	"math/rand"
 	"url-shortener/internal/model"
 )
 
@@ -20,4 +21,15 @@ func NewURLService(urlRepo URLRepositoryInterface) *URLServiceStruct {
 	}
 }
 
-func (s *URLServiceStruct) CreateURL(ctx context.Context, params model.CreateURLParams) {}
+func (s *URLServiceStruct) CreateURL(ctx context.Context, params model.CreateURLParams) {
+	_, _ = GenerateShortURL(params.URL)
+}
+
+func GenerateShortURL(url string) (string, string) {
+	shortURL := make([]byte, 6)
+	base62 := "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+	for idx := range 6 {
+		shortURL[idx] = base62[rand.Intn(len(base62))]
+	}
+	return url, string(shortURL)
+}
